b2e5e538eb9e4fbedf78956ffebe6d9bc181238b
Putra <ma.syaputra21@gmail.com>
2025-03-17 11:35:40 +0700

    icon

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,15 +7,12 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
     <title>Stepup Laundry - Premium Care</title>
-    <link rel="stylesheet" type="text/css" href="styles/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <link rel="stylesheet" type="text/css" href="custom.css">
-    <script
-            src="https://code.jquery.com/jquery-3.7.1.min.js"
-            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-            crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="{{ asset('styles/bootstrap.min.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('styles/style.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('styles/custom.css') }}">
+    <script src="{{ asset('jquery/jquery.js') }}"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="fonts/css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('fonts/css/fontawesome-all.min.css')}}">
     <link rel="manifest" href="_manifest.json">
     <link rel="apple-touch-icon" sizes="180x180" href="images/logo.png">
 
